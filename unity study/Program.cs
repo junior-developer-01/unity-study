8a79376b3fa84a7dcad3a4863e6c1f1fef40a20b
junior-developer-01 <omyhyun123@gmail.com>
2025-11-24 15:47:37 +0900

    init

--- unity study/Program.cs
+++ unity study/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //반복문
+
+            //while
+            //for
+            //
+
+
+
+            //int a = 0;
+
+            //while (a < 5) // 무한루프
+            //{
+            //    Console.WriteLine("무한");
+            //    a++;
+            //}
+
+
+
+
+            // 1. 정수 변수 num 1로 초기화한다.
+
+            // While 문으로 1부터 10까지 출력
+            // 출력 후 num을 1씩 증가.
+
+
+            int num = 1;
+
+            while (num <= 10) // 무한루프
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+
         }
     }
 }
