7e1bbf30d287844a79587908d6510adabe279187
junior-developer-01 <omyhyun123@gmail.com>
2025-11-24 14:47:16 +0900

    init

---
+++ unity study/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace unity_study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
